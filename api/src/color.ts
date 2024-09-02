04b39367844178c8d20a855d39a98afa425e153d
kevinchatham <40923272+kevinchatham@users.noreply.github.com>
2024-09-02 13:02:31 -0500

    cleanup

---
+++ api/src/color.ts
@@ -0,0 +1,6 @@
+export enum Color {
+  Reset = '\x1b[0m',
+  Bold = '\x1b[1m',
+  Underline = '\x1b[4m',
+  Orange = '\x1b[38;2;255;165;0m',
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -2,25 +2,22 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import rootRoutes from './routes/root.routes';
-
-enum AnsiColor {
-  Reset = '\x1b[0m',
-  Bold = '\x1b[1m',
-  Underline = '\x1b[4m',
-  Orange = '\x1b[38;2;255;165;0m',
-}
+import { Color } from './color';
 
 const app = new Hono();
+const port = 3000;
 
 app.use(logger());
 app.route('/', rootRoutes);
 
-const port = 3000;
 serve({
   fetch: app.fetch,
   port,
 });
 
-console.log(`
-${AnsiColor.Orange}${AnsiColor.Bold} Hono @ ${AnsiColor.Underline}http://localhost:${port}${AnsiColor.Reset} ${AnsiColor.Orange}->${AnsiColor.Reset} ️‍🔥️‍🔥️‍🔥 ${AnsiColor.Reset}
-`);
+const message =
+  `${Color.Orange}${Color.Bold} Hono @ ` +
+  `${Color.Underline}http://localhost:${port}${Color.Reset} ` +
+  `${Color.Orange}->${Color.Reset} ️‍🔥️‍🔥️‍🔥 ${Color.Reset}`;
+
+console.log(message);
